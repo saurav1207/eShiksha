5aeb7d5b7385697339b8b0b8bd12ec7da1ad652c
saurav1207 <sauravkumar843114@gmail.com>
2023-09-20 01:02:33 +0530

    Updated Code

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -21,25 +21,3 @@ const CallToAction = () => {
 export default CallToAction;
 
 
-
-
-
-
-
-// import React from 'react'
-// import { Link } from 'react-router-dom';
-// import './CallToAction.css';
-
-// const CallToAction = () => {
-//   return (
-//     <section className="call-to-action">
-//     <div className="cta-content">
-//       <h2 className="cta-title">Welcome to our eShiksha Website!</h2>
-//       <p className="cta-description">Explore our courses and services to enhance your learning experience.</p>
-//       <Link to="/course" className="cta-button">Explore Courses</Link>
-//     </div>
-//   </section>
-//   )
-// }
-
-// export default CallToAction
