b8018e5539bb791c9442607cee0f7c7e704976aa
saurav1207 <sauravkumar843114@gmail.com>
2023-09-19 22:08:51 +0530

    Added Search Functionality

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Course.css';
 import WebDevImage from '../assets/undraw_web_developer_re_h7ie.svg';
@@ -6,6 +6,8 @@ import MLImage from '../assets/undraw_source_code_re_wd9m.svg';
 import CloudImage from '../assets/undraw_cloud_hosting_7xb1.svg';
 
 const Course = () => {
+  const initialQuery = ''; 
+  const [query, setQuery] = useState(initialQuery);
   const courses = [
     {
       id: 1,
@@ -26,24 +28,51 @@ const Course = () => {
       image: CloudImage,
     },
   ];
+  
+  // Function to handle search input change
+  const handleSearchChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  // Function to filter courses based on the search query
+  const filteredCourses = courses.filter((course) =>
+    course.title.toLowerCase().includes(query.toLowerCase())
+  );
 
   return (
     <div className="course-section">
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search for courses"
+          value={query}
+          onChange={handleSearchChange}
+        />
+      </div>
       <div className="course-container">
-        {courses.map((course, index) => (
-          <div className={`course-card ${index % 2 === 0 ? 'course-section-even' : 'course-section-odd'}`} key={course.id}>
-            <div className="course-details">
-              <h2 className="course-title">{course.title}</h2>
-              <p className="course-description">{course.description}</p>
-              <Link to={`/course/${course.id}`} className="course-button">
-                Let's Learn
-              </Link>
+        {filteredCourses.length === 0 ? (
+          <p className="no-courses-message">No courses found.</p>
+        ) : (
+          filteredCourses.map((course, index) => (
+            <div
+              className={`course-card ${
+                index % 2 === 0 ? 'course-section-even' : 'course-section-odd'
+              }`}
+              key={course.id}
+            >
+              <div className="course-details">
+                <h2 className="course-title">{course.title}</h2>
+                <p className="course-description">{course.description}</p>
+                <Link to={`/course/${course.id}`} className="course-button">
+                  Let's Learn
+                </Link>
+              </div>
+              <div className="course-image">
+                <img src={course.image} alt={course.title} />
+              </div>
             </div>
-            <div className="course-image">
-              <img src={course.image} alt={course.title} />
-            </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
