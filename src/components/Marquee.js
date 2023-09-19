ee14feaa7631f2de914ec09f1de61bc9d7e79867
saurav1207 <sauravkumar843114@gmail.com>
2023-09-19 17:47:07 +0530

    Updated Code

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -2,42 +2,43 @@ import React, { useRef } from 'react';
 import '../styles/Marquee.css';
 
 const Marquee = ({ announcements }) => {
-  const marqueeRef = useRef(null);
+    const marqueeRef = useRef(null);
 
-  const handleMouseEnter = () => {
-    if (marqueeRef.current) {
-      marqueeRef.current.stop();
-    }
-  };
+    const handleMouseEnter = () => {
+        if (marqueeRef.current) {
+            marqueeRef.current.stop();
+        }
+    };
 
-  const handleMouseLeave = () => {
-    if (marqueeRef.current) {
-      marqueeRef.current.start();
-    }
-  };
+    const handleMouseLeave = () => {
+        if (marqueeRef.current) {
+            marqueeRef.current.start();
+        }
+    };
 
-  return (
-    <div className="marquee-container" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <marquee
-        ref={marqueeRef}
-        direction="up"
-        behavior="scroll"
-        scrollamount="1"
-        style={{ height: '180px', whiteSpace: 'normal' }}
-        className="latestPart"
-      >
-        {announcements.map((announcement, index) => (
-          <p key={index}>
-            ➡️
-            <content style={{ color: '#012B55' }}>{announcement.title}&nbsp;</content>
-            <a href={announcement.link} className="color-text" target="_blank">
-              <strong>Read More&nbsp;</strong>
-            </a>
-          </p>
-        ))}
-      </marquee>
-    </div>
-  );
+    return (
+        <div className="marquee-container" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <marquee
+                ref={marqueeRef}
+                direction="up"
+                behavior="scroll"
+                scrollamount="1"
+                style={{ height: '180px', whiteSpace: 'normal' }}
+                className="latestPart"
+            >
+                {announcements.map((announcement, index) => (
+                    <p key={index}>
+                        ➡️
+                        <content style={{ color: '#012B55' }}>{announcement.title}&nbsp;</content>
+                        <a href={announcement.link} className="color-text" target="_blank" rel="noopener noreferrer">
+                            <strong>Read More&nbsp;</strong>
+                        </a>
+
+                    </p>
+                ))}
+            </marquee>
+        </div>
+    );
 };
 
 export default Marquee;
